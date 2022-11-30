bfb6f2f800ed7c40047652df83eee9c5a4c9b801
aismail83 <63312291+aismail83@users.noreply.github.com>
2022-11-30 16:10:45 +0100

    Update AsyncTest.java

--- src/main/java/async/AsyncTest.java
+++ src/main/java/async/AsyncTest.java
@@ -21,24 +21,77 @@ class AsyncTest {
       new Ceo("ceo_2", "Sundar"),
       new Ceo("ceo_3", "Bill")
   );
-  public static void setCeos(String ceo_id , String ceoNom){
-     this.ceos = List.of(new Ceo(ceo_id , ceoNom);
-  }
-
-  public static void setEnterprises(String ent_id,String entrepriseNom,String ceo_id){
-      this.enterprises = List.of(new Enterprise(ent_id , entrepriseNom , ceo_id);
-  }
-  
+   
   public static CompletableFuture<Option<Ceo>> getCeoById(String ceo_id) {
-    return null;
+    Option<Ceo> optionCeo;
+    for (Ceo ceo : ceos){
+            if (ceo.getceo_id().equalsIgnoreCase(ceo-id)){
+               optionCeo = Option.of(ceo);
+            }
+            else{
+              optionCeo = Option.none;
+              
+            }
+              
+        } 
+     return CompletableFuture.supplyAsync(() -> optionCeo);
   }
+    
+   
 
   public static CompletableFuture<Option<Enterprise>> getEnterpriseByCeoId(String ceo_id) {
-    return null;
+    Option<Entreprise> optionEnt;
+    Tuple2<Option<Ceo>, Option<Entreprise>> tupl2ceoent;
+
+     for (Entreprise entreprise : entreprises){
+            if (entreprise.getceo_id().equalsIgnoreCase(ceo-id)){
+               Option<Entreprise> optionEnt = Option.of(entreprise);
+            }
+            else{
+              optionEnt = Option.None;
+              
+            }
+    
+              
+        } 
+    
+    return CompletableFuture.supplyAsync(() -> optionEnt);
   }
 
   public static CompletableFuture<Tuple2<Option<Ceo>, Option<Enterprise>>> getCEOAndEnterprise(String ceo_id) {
-    return null;
+    Option<Entreprise> optionEnt;
+    Option<Ceo> optionCeo;
+    Tuple2<Option<Ceo>, Option<Entreprise>> tupl2Eeoent;
+     for (Entreprise entreprise : entreprises){
+            if (entreprise.getceo_id().equalsIgnoreCase(ceo-id)){
+               Option<Entreprise> optionEnt = Option.of(entreprise);
+            }
+            else{
+              optionEnt = Option.none;
+              
+            }
+              
+        } 
+    for (Ceo ceo : ceos){
+            if (ceo.getceo_id().equalsIgnoreCase(ceo-id)){
+               optionCeo = Option.of(ceo);
+            }
+            else{
+              optionCeo = Option.none;
+              
+            }
+      
+              
+        } 
+   if(optionCeo == Option.None){
+        tupl2CeoEnt =Tuple.of(None,None);
+      }
+   else{
+      tupl2CeoEnt =Tuple.of(optionCeo,optionEnt);
+      
+    }
+    return CompletableFuture.supplyAsync(() -> tupl2CeoEnt);
+    
   }
 
 }
