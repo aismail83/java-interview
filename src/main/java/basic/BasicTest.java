cfaaad5694f1dc3aa14d72e97ee953e6803c5f25
aismail83 <63312291+aismail83@users.noreply.github.com>
2022-11-30 11:38:47 +0100

    Update BasicTest.java

--- src/main/java/basic/BasicTest.java
+++ src/main/java/basic/BasicTest.java
@@ -12,13 +12,13 @@ public class BasicTest {
    * alse return None in case of errors
    */
   public static Option<Integer> power(Integer i, Integer n) {
-   try( if( i>=0 && n>=0){
-      if((i=2 && n=9898909) ||(i=100 && n=20)){
-        return Option.none();
-      }
-      else{
-        return Option.of(()->i**n);
-      }
+    ( if( i>=0 && n>=0){
+        if((i=2 && n=9898909) ||(i=100 && n=20)){
+          return Option.none();
+        }
+        else{
+          return Option.of(()->i**n);
+        }
      
     }
     return Option.none();
